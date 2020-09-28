7f844c356da2d9bbeba95bc83379cdbcf1e61605
PavloTurchyn <pavlo.t@centiva.health>
2020-09-28 20:44:35 +0300

    add unit tests

--- promo/src/app/core/services/validation/validation.service.spec.ts
+++ promo/src/app/core/services/validation/validation.service.spec.ts
@@ -1,16 +1,24 @@
+import { ValidationService } from './validation.service';
 import { TestBed } from '@angular/core/testing';
 
-import { ValidationService } from './validation.service';
+let validationService: ValidationService;
 
-describe('ValidationService', () => {
-	let service: ValidationService;
-
-	beforeEach(() => {
-		TestBed.configureTestingModule({});
-		service = TestBed.inject(ValidationService);
-	});
-
-	it('should be created', () => {
-		expect(service).toBeTruthy();
-	});
+describe('Validation service', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ValidationService],
+    });
+    validationService = TestBed.inject(ValidationService);
+  });
+  it('setValidators method should be defined', () => {
+    expect(validationService.setValidators).toBeDefined();
+  });
+  it('setValidators method should return array', () => {
+    const validatorFnArray = validationService.setValidators('random string');
+    expect(validatorFnArray).toEqual([]);
+  });
+  it('setValidators method should return not empty array when type is email', () => {
+    const validatorFnArray = validationService.setValidators('email');
+    expect(validatorFnArray).not.toEqual([]);
+  });
 });
--- promo/src/app/pages/item/item.component.spec.ts
+++ promo/src/app/pages/item/item.component.spec.ts
@@ -1,25 +1,38 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ItemComponent } from './item.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DataSharingService } from '../../core/services/data-sharing/data-sharing.service';
+import { ActivatedRoute } from '@angular/router';
 
+let component: ItemComponent;
+let fixture: ComponentFixture<ItemComponent>;
+let h3: HTMLElement;
 describe('ItemComponent', () => {
-  let component: ItemComponent;
-  let fixture: ComponentFixture<ItemComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ItemComponent ]
-    })
-    .compileComponents();
-  }));
+  let mockActivatedRoute = {
+    snapshot: {
+      params: {
+        id: 1,
+      },
+    },
+  };
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ItemComponent],
+      providers: [
+        DataSharingService,
+        {
+          provide: ActivatedRoute,
+          useValue: mockActivatedRoute,
+        },
+      ],
+    });
     fixture = TestBed.createComponent(ItemComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.index = 1;
+    h3 = fixture.nativeElement.querySelector('h3');
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display title "My index is 1"', () => {
+    fixture.detectChanges();
+    expect(h3.textContent).toContain('My index is 1');
   });
 });
